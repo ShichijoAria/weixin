83e3d367ad56524abc1778a9ff49c3c9a9a8733d
aria <15608176098@163.com>
2018-12-01 20:47:31 +0800

    merge httpUtil

--- src/main/java/org/yorha/weixin/controller/CarNumServlet.java
+++ src/main/java/org/yorha/weixin/controller/CarNumServlet.java
@@ -27,7 +27,7 @@ import java.util.Map;
 public class CarNumServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        req.getRequestDispatcher("/WEB-INF/index2.html").forward(req,resp);
+        req.getRequestDispatcher("/WEB-INF/carNum.html").forward(req,resp);
     }
 
 
